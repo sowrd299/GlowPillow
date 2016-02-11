8d6ccc5409f3ccf6b6185abbb996fdbdb7368ff6
sowrd299 <ryan.ward46@gmail.com>
2016-02-10 23:38:03 -0800

    Vignette Tweek
    Adjusted throbbing algorythm to throb faster as value increases.

--- Assets/Scripts/UI/VignetteController.cs
+++ Assets/Scripts/UI/VignetteController.cs
@@ -12,8 +12,8 @@ public class VignetteController : MonoBehaviour {
 	void Update () {
 		//Debug.Log("Setting vingette to: "+getHealth().ToString()+"; Health is: "+pStats.curHealth.ToString());
 		image.color = new Color (image.color.r, image.color.g, image.color.b,
-			getVal () - 0.25f * getVal() * Mathf.Abs(Mathf.Sin(4.0f * Time.fixedTime/Mathf.PI)));
-		//throbs at a rate of 120 bmp. can be adjusted/made more sofistigated.
+			getVal () - 0.25f * getVal() * Mathf.Abs(Mathf.Sin((4.0f + 4.0f*getVal()) * Time.fixedTime/Mathf.PI)));
+		//throbs at a rate of 120-240 bmp, based on health
 	}
 
 	private float getVal(){
